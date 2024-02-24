9978fc1d07a6140b386138c4294a8a34f91ba570
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-24 15:02:36 -0600

    added figure generation for task 1

---
+++ task1.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+
+def make_plot():
+    '''
+    Function to plot the average FVAF values for each training set size
+    Lines for training, validation, and test sets shown
+    '''
+    # Values used for task 1
+    rotations = range(0, 20, 2)
+    Ntraining = [1, 2, 4, 6, 9, 13, 18]
+
+    # Create numpy arrays for each set
+    fvafs_training = np.empty((len(rotations), len(Ntraining)))
+    fvafs_validation = np.empty((len(rotations), len(Ntraining)))
+
+    # Loop over each experiment
+    for i, r in rotations:
+        for j, n in enumerate(Ntraining):
+            # Open experiment results and add them to arrays
+            with open(f'base__ddtheta_1_hidden_500_250_125_75_36_17_JI_rotation_{r}_Ntraining_{n}_results.pkl', "rb") as fp:
+                results = pickle.load(fp)
+                fvafs_training[i][j] = results['predict_training_fvaf']
+                fvafs_validation[i][j] = results['predict_validation_fvaf']
+
+    # Compute average FVAF for each training set size for each set
+    avg_fvafs_training = np.average(fvafs_training, axis=0)
+    avg_fvafs_validation = np.average(fvafs_validation, axis=0)
+
+    # Create line plot
+    fig = plt.figure()
+    plt.plot(Ntraining, avg_fvafs_training, label='training')
+    plt.plot(Ntraining, avg_fvafs_validation, label='validation')
+    plt.ylabel('FVAF')
+    plt.xlabel('Training Folds')
+    plt.title('FVAF vs Training Folds')
+    plt.legend()
+    fig.savefig('task1.png')
+
+
+if __name__ == '__main__':
+    make_plot()
