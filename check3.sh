cd37cbacd380637b4ca238fb5d24072fef41e202
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-24 15:52:24 -0600

    added check 3 batch

---
+++ check3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Brandon Michaud
+#
+# disc_dual_a100_students
+#SBATCH --partition=disc_dual_a100_students
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task 16
+#SBATCH --mem=1G
+#SBATCH --output=outputs/hw2_%j_stdout.txt
+#SBATCH --error=outputs/hw2_%j_stderr.txt
+#SBATCH --time=00:05:00
+#SBATCH --job-name=hw2
+#SBATCH --mail-user=brandondmichaud@ou.edu
+#SBATCH --mail-type=ALL
+#SBATCH --chdir=/home/cs504319/cs5043-hw2
+
+. /home/fagg/tf_setup.sh
+conda activate tf
+
+python hw2_base.py --project 'hw2' --exp_type 'l2' --output_type 'ddtheta' --predict_dim 1 --check --lrate 0.0001 --activation_hidden 'elu' --activation_out 'linear' --hidden 500 250 125 75 36 17 --epochs 100 -vv
