143f0f30d80198c2e2af7a29b5e555fe7cbac9cb
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-24 17:35:25 -0600

    added code for figure 3

--- task3.sh
+++ task3.sh
@@ -13,7 +13,7 @@
 #SBATCH --mail-user=brandondmichaud@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504319/cs5043-hw2
-#SBATCH --array=102,119,201
+# --array=102,119,201
 
 . /home/fagg/tf_setup.sh
 conda activate tf
