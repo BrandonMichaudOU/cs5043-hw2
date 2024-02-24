60cac482f84285a7fda6ba9714c58a1592ade6c0
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-24 00:02:32 -0600

    fixed batch file

--- task1.sh
+++ task1.sh
@@ -6,16 +6,15 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task 16
 #SBATCH --mem=1G
-#SBATCH --output=outputs/hw1_%j_stdout.txt
-#SBATCH --error=outputs/hw1_%j_stderr.txt
+#SBATCH --output=outputs/hw2_%j_stdout.txt
+#SBATCH --error=outputs/hw2_%j_stderr.txt
 #SBATCH --time=00:05:00
-#SBATCH --job-name=hw1
+#SBATCH --job-name=hw2
 #SBATCH --mail-user=brandondmichaud@ou.edu
 #SBATCH --mail-type=ALL
-#SBATCH --chdir=/home/cs504319/cs5043-hw1
+#SBATCH --chdir=/home/cs504319/cs5043-hw2
 
 . /home/fagg/tf_setup.sh
 conda activate tf
 
 python hw2_base.py --project 'hw2' --exp_type 'base' --output_type 'ddtheta' --predict_dim 1 --rotation 10 --Ntraining 18 --lrate 0.0001 --activation_hidden 'elu' --activation_out 'linear' --hidden 500 250 125 75 36 17 --epochs 100 --min_delta 0.001 --patience 25 -vv
-python task1.py
