d9847f61b4235bc5c30916266943e38e809c2cb6
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-26 16:32:09 -0600

    updated t-test

--- task4.py
+++ task4.py
@@ -86,10 +86,10 @@ def make_plot():
     print(f'For base and dropout pair\npvalue = {t_base_dropout_1.pvalue}, '
           f'diff = {np.abs(np.mean(fvafs_testing_base[:][0]) - np.mean(fvafs_testing_dropout[:][0]))}')
     t_base_l2_1 = scipy.stats.ttest_rel(fvafs_testing_base[:][0], fvafs_testing_l2[:][0])
-    print(f'For base and dropout pair\npvalue = {t_base_l2_1.pvalue}, '
+    print(f'For base and L2 pair\npvalue = {t_base_l2_1.pvalue}, '
           f'diff = {np.abs(np.mean(fvafs_testing_base[:][0]) - np.mean(fvafs_testing_l2[:][0]))}')
     t_dropout_l2_1 = scipy.stats.ttest_rel(fvafs_testing_dropout[:][0], fvafs_testing_l2[:][0])
-    print(f'For base and dropout pair\npvalue = {t_dropout_l2_1.pvalue}, '
+    print(f'For dropout and L2 pair\npvalue = {t_dropout_l2_1.pvalue}, '
           f'diff = {np.abs(np.mean(fvafs_testing_dropout[:][0]) - np.mean(fvafs_testing_l2[:][0]))}')
 
     print(f'\nt-tests for 18 training folds')
@@ -97,12 +97,12 @@ def make_plot():
     print(f'For base and dropout pair\npvalue = {t_base_dropout_18.pvalue}, '
           f'diff = {np.abs(np.mean(fvafs_testing_base[:][6]) - np.mean(fvafs_testing_dropout[:][6]))}')
     t_base_l2_18 = scipy.stats.ttest_rel(fvafs_testing_base[:][6], fvafs_testing_l2[:][6])
-    print(f'For base and dropout pair\npvalue = {t_base_l2_18.pvalue}, '
+    print(f'For base and L2 pair\npvalue = {t_base_l2_18.pvalue}, '
           f'diff = {np.abs(np.mean(fvafs_testing_base[:][6]) - np.mean(fvafs_testing_l2[:][6]))}')
     t_dropout_l2_18 = scipy.stats.ttest_rel(fvafs_testing_dropout[:][6], fvafs_testing_l2[:][6])
-    print(f'For base and dropout pair\npvalue = {t_dropout_l2_18.pvalue}, '
+    print(f'For dropout and L2 pair\npvalue = {t_dropout_l2_18.pvalue}, '
           f'diff = {np.abs(np.mean(fvafs_testing_dropout[:][6]) - np.mean(fvafs_testing_l2[:][6]))}')
-    
+
 
 if __name__ == '__main__':
     make_plot()
