9391fbbbbc0e5539f9d04017b3c411a3a83af44f
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-26 15:07:23 -0600

    updated t-test

--- task4.py
+++ task4.py
@@ -81,7 +81,14 @@ def make_plot():
     fig.savefig('task4.png')
 
     # Run t-tests
-    print(scipy.stats.ttest_rel(fvafs_testing_base[:][0], fvafs_testing_dropout[:][0]))
+    t_base_dropout_1 = scipy.stats.ttest_rel(fvafs_testing_base[:][0], fvafs_testing_dropout[:][0])
+    t_base_l2_1 = scipy.stats.ttest_rel(fvafs_testing_base[:][0], fvafs_testing_l2[:][0])
+    t_dropout_l2_1 = scipy.stats.ttest_rel(fvafs_testing_dropout[:][0], fvafs_testing_l2[:][0])
+    print(t_base_dropout_1.pvalue)
+    t_base_dropout_18 = scipy.stats.ttest_rel(fvafs_testing_base[:][6], fvafs_testing_dropout[:][6])
+    t_base_l2_18 = scipy.stats.ttest_rel(fvafs_testing_base[:][6], fvafs_testing_l2[:][6])
+    t_dropout_l2_18 = scipy.stats.ttest_rel(fvafs_testing_dropout[:][6], fvafs_testing_l2[:][6])
+
 
 
 if __name__ == '__main__':
