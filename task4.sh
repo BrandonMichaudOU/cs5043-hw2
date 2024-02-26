64a6b0f03a880fb8d606409b37573514075ef2bd
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-26 16:57:21 -0600

    updated t-test

--- task4.sh
+++ task4.sh
@@ -2,7 +2,7 @@
 # Brandon Michaud
 #
 # disc_dual_a100_students
-#SBATCH --partition=disc_dual_a100_students
+#SBATCH --partition=debug_5min
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task 16
 #SBATCH --mem=1G
